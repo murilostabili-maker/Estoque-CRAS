659bb6c8576a97f855395a95d2c4b298b8dd0bf4
murilostabili-maker <murilo.stabili@academico.ufpb.br>
2026-08-09 13:48:28 -0300

    Adjust column width in app.py

--- app.py
+++ app.py
@@ -690,7 +690,7 @@ def main():
                 "<p style='text-align:center; color:gray; font-size:0.7rem; margin-bottom:0.2rem;'>Desenvolvido por</p>",
                 unsafe_allow_html=True
             )
-            sub_esq, sub_dir = st.columns([1, 18])
+            sub_esq, sub_dir = st.columns([1, 22])
             with sub_dir:
                 logo(LOGO_GESP, width=55)
 
