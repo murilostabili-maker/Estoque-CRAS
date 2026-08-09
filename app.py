d5c03c05e72e5b8e70fe246cdb3a7c58447fd640
murilostabili-maker <murilo.stabili@academico.ufpb.br>
2026-08-09 13:42:43 -0300

    Equalize column widths in layout

--- app.py
+++ app.py
@@ -737,7 +737,7 @@ def main():
                 "<p style='text-align:center; color:gray; font-size:0.7rem; margin-bottom:0.2rem;'>Desenvolvido por</p>",
                 unsafe_allow_html=True
             )
-            sub_esq, sub_dir = st.columns([2, 1])
+            sub_esq, sub_dir = st.columns([1, 1])
             with sub_dir:
                 logo(LOGO_GESP, width=55)
 
