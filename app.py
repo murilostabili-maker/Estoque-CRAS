b698147751f5f779854ee27ad50cf15ddfe8fcbe
Joao-Toscano <joao.toscano@mindsight.com.br>
2026-08-26 00:30:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -747,7 +747,7 @@ def main():
                 "<p style='text-align:center; color:gray; font-size:0.7rem; margin-bottom:0.2rem;'>Desenvolvido por</p>",
                 unsafe_allow_html=True
             )
-            sub_esq, sub_centro, sub_dir = st.columns([1, 8, 2]) 
+            sub_esq, sub_centro, sub_dir = st.columns([1, 10, 2]) 
             with sub_centro:
                 logo(LOGO_GESP, width=55)
 
