7424e6c26f6bcbde0f4b5b934c68b0a12c5edf5e
Joao-Toscano <joao.toscano@mindsight.com.br>
2026-08-26 00:08:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def tela_login():
     
     with col_centro:
         # 2. Centralizando a Logo Principal
-        col_logo_esq, col_logo_centro, col_logo_dir = st.columns([1, 1, 1])
+        col_logo_esq, col_logo_centro, col_logo_dir = st.columns([3, 1, 3])
         with col_logo_centro:
             logo(LOGO_CRAS, width=150)
 
@@ -90,7 +90,7 @@ def tela_login():
         )
         
         # Centralizando a Logo do Rodapé (Removido o [1, 4] assimétrico)
-        col_foot_esq, col_foot_centro, col_foot_dir = st.columns([1, 1, 1])
+        col_foot_esq, col_foot_centro, col_foot_dir = st.columns([3, 1, 3])
         with col_foot_centro:
             logo(LOGO_GESP, width=70)
             
